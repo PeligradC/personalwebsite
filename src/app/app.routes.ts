b4ff90ebe5227baf963947b4df3158606ef251d3
Peligrad Cristian <peligradcristian5721157@gmail.com>
2025-01-20 11:20:02 +0200

    testet list functionality with clodinary

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'add-product', component: AddProductComponent},
+    {path: 'edit-product/:id', component: EditProductComponent},
+    {path: '', component: ListProductComponent},
+    {path: '',redirectTo:'',pathMatch:'full'},
+];
