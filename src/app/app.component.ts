14a5db0870d0123b6e9687edf5096380121c6288
Peligrad Cristian <peligradcristian5721157@gmail.com>
2025-06-23 05:14:09 +0300

    added front for register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,15 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
+//import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HttpClientModule,ReactiveFormsModule,CommonModule,RouterModule],
+  imports: [RouterOutlet, NavbarComponent, HttpClientModule,ReactiveFormsModule,CommonModule,RouterModule,FormsModule
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { EditProductComponent } from './components/edit-product/edit-product.component';
 import { ListProductComponent } from './components/list-product/list-product.component';
-import { LoginComponent } from './loginregister/login/login.component';
+import { RegisterComponent } from './loginregister/register/register.component';
 
 export const routes: Routes = [
     { 
@@ -11,5 +11,5 @@ export const routes: Routes = [
     },
     { path: 'add-product', component: AddProductComponent },
     { path: 'edit-product/:id', component: EditProductComponent },
-    {path: 'login', component: LoginComponent}
+    {path: 'register', component: RegisterComponent}
   ];
